1c549e2e624c5d74ea0f8e5fe3e1c64ce2fbb180
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-06-01 10:10:25 +0900

    Add files via upload

---
+++ home.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.write('# Hi! Welcome to My App!')
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
+
+option = st.selectbox(
+    '좋아하는 동물은?',
+    ('강아지', '고양이', '양', '토끼', '코끼리'))
+
+st.write('내가 좋아하는 동물은', option, '입니다.')
+st.write(f'좋아하는 동물은 {option} 입니다.')
+
+txt = st.text_area('자신을 소개해보세요.', '''
+    
+''')
+st.write('입력한 내용은:', txt)
+
+age = st.slider('나이를 선택하세요', 0, 130, 25)
+st.write("저의 나이는", age, '입니다.')
+
