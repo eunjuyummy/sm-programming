efbff35c825162ab262287f18d4652f35d39019d
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-06-11 00:43:21 +0900

    Update home.py

--- test/home.py
+++ test/home.py
@@ -3,24 +3,20 @@ import pandas as pd
 
 # Assuming you have the recommended_movies DataFrame
 # Read the movie data
-movies_df = pd.read_csv('test/movie_data.csv')
+movie_df = pd.read_csv('test/movie_data.csv')
 
 # Streamlit application settings
 st.title('영화 추천 시스템')
 st.write('영화를 선택하세요.')
 
 # User selects a movie
-selected_movie = st.selectbox('영화 선택', movies_df['title'].unique())
+selected_movie = st.selectbox('영화 선택', movie_df['title'].unique())
 
-# Filter the selected movie from the DataFrame
-selected_movie_data = recommended_movies[movies_df['title'] == selected_movie]
+movie_df = movie_df[movie_df['title'] != selected_movie]
 
-# Display the selected movie's details
-if not selected_movie_data.empty:
-    st.subheader('선택한 영화')
-    st.write(selected_movie_data)
+matching_movies = movie_df[movie_df['parsed_genres'] == selected_movie]['title']
 
-# Display the recommended movies
-st.subheader('추천 영화')
-#for index, row in recommended_movies.iterrows():
-#    st.write('- 제목:', row['영화 제목'], '| 평점:', row['평점'])
+# Display the matching movies
+st.subheader('일치하는 영화')
+for movie in matching_movies:
+    st.write('- 영화 제목:', movie)
