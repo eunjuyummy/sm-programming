f662d02bf9019b530603d0fe18a1b3bdfd68d867
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-06-11 02:13:24 +0900

    Update home.py

--- test/home.py
+++ test/home.py
@@ -13,10 +13,17 @@ st.write('영화를 선택하세요.')
 def button_clicked():
     st.write('Image button clicked!')
 
-# Image button creation
-button_image = st.button(label='Click me!', help='Image button', on_click=button_clicked)
-# Image button with an image
-button_image.image('test/image/apollo13.jpg', caption='Image button')
+# Load the image
+image = Image.open('test/image/apollo13.jpg')
+
+# Display the image and button
+col_image, col_button = st.beta_columns([1, 1])
+with col_image:
+    st.image(image, use_column_width=True)
+
+with col_button:
+    if st.button('Click me!', help='Image button', on_click=button_clicked):
+        pass
 
 
 #image = Image.open('sunrise.jpg')
