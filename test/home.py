333e2fbf6de823afc13fc7b67024913ab6727678
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-06-11 00:31:19 +0900

    Update home.py

--- test/home.py
+++ test/home.py
@@ -17,6 +17,6 @@ selected_genre = movies_df[movies_df['title'] == selected_movie]['parsed_genres'
 # 같은 장르의 영화 중 평점이 높은 상위 3개를 추천한다
 recommended_movies = movies_df[(movies_df['parsed_genres'] == selected_genre).nlargest(3, 'rating')
 
-# 추천 영화를 출력한다
+# 추천 영화를 출력
 for index, row in recommended_movies.iterrows():
     st.write('- 제목:', row['title'], '| 평점:', row['rating'])
