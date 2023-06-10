ef6ff73ce858a5594e10978fb3161a8c9bcb5280
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-06-11 00:34:23 +0900

    Update home.py

--- test/home.py
+++ test/home.py
@@ -1,22 +1,26 @@
 import streamlit as st
 import pandas as pd
 
-# 영화 데이터를 읽어온다
-movies_df = pd.read_csv('test/movie_data.csv')
+# Assuming you have the recommended_movies DataFrame
+# Read the movie data
+movies_df = pd.read_csv('영화데이터.csv')
 
-# Streamlit 애플리케이션 설정
+# Streamlit application settings
 st.title('영화 추천 시스템')
 st.write('영화를 선택하세요.')
 
-# 사용자에게 영화 선택을 받는다
+# User selects a movie
 selected_movie = st.selectbox('영화 선택', movies_df['title'].unique())
 
-# 선택한 영화의 장르를 확인한다
-selected_genre = movies_df[movies_df['title'] == selected_movie]['parsed_genres'].values[0]
+# Filter the selected movie from the DataFrame
+selected_movie_data = recommended_movies[movies_df['parsed_genres'] == selected_movie]
 
-# 같은 장르의 영화 중 평점이 높은 상위 3개를 추천한다
-recommended_movies = movies_df[(movies_df['parsed_genres'] == selected_genre).nlargest(3, 'rating')
+# Display the selected movie's details
+if not selected_movie_data.empty:
+    st.subheader('선택한 영화')
+    st.write(selected_movie_data)
 
-# 추천 영화를 출력
+# Display the recommended movies
+st.subheader('추천 영화')
 for index, row in recommended_movies.iterrows():
-    st.write('- 제목:', row['title'], '| 평점:', row['rating'])
+    st.write('- 제목:', row['영화 제목'], '| 평점:', row['평점'])
