54b1342accd8eef3f58fb75ab4757159461a6cc9
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-06-11 01:17:39 +0900

    Update home.py

--- test/home.py
+++ test/home.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-# Assuming you have the recommended_movies DataFrame
 # Read the movie data
 movie_df = pd.read_csv('test/movie_data.csv')
 
@@ -9,18 +8,37 @@ movie_df = pd.read_csv('test/movie_data.csv')
 st.title('영화 추천 시스템')
 st.write('영화를 선택하세요.')
 
-selected_movie = []
-# User selects a movie
-for i in range(3):
-    selected_movie[i] = st.selectbox('영화 선택', movie_df['title'].unique())
+# User selects three movies
+selected_movies = st.multiselect('영화 선택', movie_df['title'].unique(), [], 3)
 
-fav_genre = movie_df[movie_df['title'] == selected_movie]['parsed_genres']
-#movie_df = movie_df[movie_df['title'] != selected_movie]
+if len(selected_movies) == 3:
+    # Initialize lists to store matching movies
+    matching_movies = []
 
-for movie in movie_df:
-    matching_movies.append(vie_df[movie_df['parsed_genres'] == fav_genre]['title'])
+    # Find favorite genres for selected movies
+    fav_genres = movie_df[movie_df['title'].isin(selected_movies)]['parsed_genres'].tolist()
 
-# Display the matching movies
-st.subheader('일치하는 영화')
-for movie in matching_movies:
-    st.write('- 영화 제목:', movie)
+    # Filter movies by favorite genres and remove selected movies from the DataFrame
+    filtered_df = movie_df[movie_df['parsed_genres'].isin(fav_genres) & ~movie_df['title'].isin(selected_movies)]
+
+    # Group movies by title and calculate the average rating
+    grouped_df = filtered_df.groupby('title').agg({'rating': 'mean'}).reset_index()
+
+    # Sort movies by rating in descending order
+    sorted_df = grouped_df.sort_values('rating', ascending=False)
+
+    # Display the top 3 movies for each favorite genre
+    st.subheader('일치하는 영화')
+
+    for genre in fav_genres:
+        # Get the top 3 movies for the current genre
+        top_movies = sorted_df[sorted_df['parsed_genres'] == genre].head(3)
+
+        if not top_movies.empty:
+            st.subheader('장르: {}'.format(genre))
+            for index, row in top_movies.iterrows():
+                st.write('- 영화 제목: {} (평점: {:.2f})'.format(row['title'], row['rating']))
+        else:
+            st.write('장르 {}에 대한 일치하는 영화가 없습니다.'.format(genre))
+else:
+    st.write('3개의 영화를 선택해야 합니다.')
