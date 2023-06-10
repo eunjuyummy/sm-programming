4af5bfdd122107fb762fc4b2cfe3b08511c58ecc
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-06-11 02:15:41 +0900

    Update home.py

--- test/home.py
+++ test/home.py
@@ -9,25 +9,24 @@ movie_df = pd.read_csv('test/movie_data.csv')
 st.title('영화 추천 시스템')
 st.write('영화를 선택하세요.')
 
-# 이미지 버튼 클릭 시 실행될 함수
-def button_clicked():
-    st.write('Image button clicked!')
-
-# Load the image
-image = Image.open('test/image/apollo13.jpg')
-
-# Display the image and button
-col_image, col_button = st.beta_columns([1, 1])
-with col_image:
-    st.image(image, use_column_width=True)
-
-with col_button:
-    if st.button('Click me!', help='Image button', on_click=button_clicked):
-        pass
-
 
 #image = Image.open('sunrise.jpg')
 
 #st.image(image, caption='Sunrise by the mountains')
 
+# User selects a movie
+selected_movie = st.selectbox('영화 선택', movie_df['title'].unique())
 
+# Get the genre of the selected movie
+selected_genre = movie_df.loc[movie_df['title'] == selected_movie, 'parsed_genres'].values[0]
+
+# Filter movies with the same genre and sort by rating
+matching_movies = movie_df.loc[movie_df['parsed_genres'] == selected_genre]
+sorted_movies = matching_movies.sort_values('rating', ascending=False).head(5)
+
+# Display the top 5 movies with the highest rating from the same genre
+st.subheader('Top 5 영화')
+for index, row in sorted_movies.iterrows():
+    st.write('- 영화 제목:', row['title'])
+    st.write('  평점:', row['rating'])
+    st.write('  장르:', row['parsed_genres'])
