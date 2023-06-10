233b98baacb093c2d94c7936fe3ddf1305c8f0f9
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-06-11 03:34:01 +0900

    Update movielens.py

--- test/movielens.py
+++ test/movielens.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 # Displaying a header with text and emojis
 st.header('All About Movies :movie_camera:')
-st.title('Movie Lens and emojis :sunglasses:')
+st.title('Movie Lens')
