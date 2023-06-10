5ba2918f36958dfdc0339a2e9bb23bdce8e6d75f
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-06-11 00:12:54 +0900

    Update home.py

--- test/home.py
+++ test/home.py
@@ -1,50 +1,23 @@
-import cv2
-import numpy as np
 import streamlit as st
-from tensorflow.keras.models import load_model
+import pandas as pd
 
-# 학습된 모델 로드
-model = load_model('your_model.h5')  # 학습된 모델 파일로 대체해야 함
+# 영화 데이터를 읽어온다
+movies_df = pd.read_csv('movie_table.csv')
 
-# 카메라로부터 이미지 캡처 함수
-def capture_image():
-    cap = cv2.VideoCapture(0)
-    _, frame = cap.read()
-    cap.release()
-    return frame
+# Streamlit 애플리케이션 설정
+st.title('영화 추천 시스템')
+st.write('영화를 선택하세요.')
 
-# 이미지 전처리 함수
-def preprocess_image(image):
-    # 이미지를 흑백으로 변환
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # 이미지 크기 조정 및 정규화
-    resized = cv2.resize(gray, (28, 28))
-    normalized = resized / 255.0
-    # 차원 추가 (모델 입력 형태로 변환)
-    processed = np.expand_dims(normalized, axis=0)
-    return processed
+# 사용자에게 영화 선택을 받는다
+selected_movie = st.selectbox('영화 선택', movies_df['영화 제목'].unique())
 
-# 손글씨 숫자 예측 함수
-def predict_digit(image):
-    processed_image = preprocess_image(image)
-    prediction = model.predict(processed_image)
-    predicted_label = np.argmax(prediction)
-    confidence = np.max(prediction) * 100
-    return predicted_label, confidence
+# 선택한 영화의 장르를 확인한다
+selected_genre = movies_df[movies_df['영화 제목'] == selected_movie]['장르'].values[0]
 
-# 앱 제목과 설명
-st.title('손글씨 숫자 맞추기')
-st.write('카메라로부터 캡처한 손글씨 이미지를 이용하여 숫자를 맞춥니다.')
+# 같은 장르의 영화 중 평점이 높은 상위 3개를 추천한다
+recommended_movies = movies_df[(movies_df['장르'] == selected_genre) & (movies_df['평점'] > 8)].nlargest(3, '평점')
 
-# 카메라로부터 이미지 캡처
-captured_image = capture_image()
-
-# 캡처한 이미지 표시
-st.image(captured_image, channels='BGR', caption='캡처한 이미지')
-
-# 숫자 예측
-predicted_digit, confidence = predict_digit(captured_image)
-
-# 예측 결과 및 신뢰도 표시
-st.write(f'예측된 숫자: {predicted_digit}')
-st.write(f'신뢰도: {confidence:.2f}%')
+# 추천 영화를 출력한다
+st.subheader('추천 영화')
+for index, row in recommended_movies.iterrows():
+    st.write('- 제목:', row['영화 제목'], '| 평점:', row['평점'])
