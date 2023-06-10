46f7f67c7611ae9a92e30364341909b85683dcf3
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-06-11 04:30:39 +0900

    Update home.py

--- test/home.py
+++ test/home.py
@@ -84,8 +84,7 @@ if add_selectbox == "Movie Recommendation":
     # Display the top 5 movies with the highest rating from the same genre
     st.subheader('Our top 5 movie picks ')
     for index, row in sorted_movies.iterrows():
-        st.subheader('Title')
-        st.write(row['title'])
+        st.write('Title: ',row['title'])
         
         
         
