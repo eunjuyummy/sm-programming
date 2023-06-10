8f7e3d9f9cb79dfcd328592f4ca0c6733a0e6cce
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-06-11 02:19:58 +0900

    Update home.py

--- test/home.py
+++ test/home.py
@@ -15,7 +15,7 @@ st.write('영화를 선택하세요.')
 #st.image(image, caption='Sunrise by the mountains')
 
 # User selects a movie
-selected_movie = st.selectbox('영화 선택', movie_df['title'].unique())
+selected_movie = st.selectbox("What's your favorite movie?", movie_df['title'].unique())
 
 # Get the genre of the selected movie
 selected_genre = movie_df.loc[movie_df['title'] == selected_movie, 'parsed_genres'].values[0]
@@ -24,8 +24,10 @@ selected_genre = movie_df.loc[movie_df['title'] == selected_movie, 'parsed_genre
 matching_movies = movie_df.loc[movie_df['parsed_genres'] == selected_genre]
 sorted_movies = matching_movies.sort_values('rating', ascending=False).head(5)
 
+st.write('Your favorite movie genre', selected_genre)
 # Display the top 5 movies with the highest rating from the same genre
-st.subheader('Top 5 영화')
+st.subheader('Our top 5 movie picks ')
+
 for index, row in sorted_movies.iterrows():
     st.write('- 영화 제목:', row['title'])
     st.write('  평점:', row['rating'])
