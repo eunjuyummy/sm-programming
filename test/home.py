dfc0cacd173e7b526c3f0b0dda9c4a53dde69ead
Eunju Kwon <101487529+eunjuyummy@users.noreply.github.com>
2023-06-12 01:03:07 +0900

    Update home.py

--- test/home.py
+++ test/home.py
@@ -149,8 +149,8 @@ else:
     # User로 부터 새로운 정보를 입력 받는 코드 
     new_title = st.text_input('title')
     
-    if(new_title == movie_df['title']):
-       print("The movie already exists.")
+    if new_title in movie_df['title'].values:
+       st.subheader('The movie already exists.')
        
     genres = movie_df['parsed_genres'].unique() 
     new_genre = st.selectbox('genres', genres)
